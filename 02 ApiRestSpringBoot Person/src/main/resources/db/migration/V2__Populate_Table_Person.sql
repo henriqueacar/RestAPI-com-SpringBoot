3c6d2b98efdd1be485e04684eea8ac226d3bb5a9
Henrique Cardoso <henrique_acar@hotmail.com>
2022-01-07 18:27:19 -0300

    Adiciona Swagger e Migrations com Flyway

---
+++ 02 ApiRestSpringBoot Person/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO `person` VALUES 
+(1,'Angra dos Reis','Henrique','Masculino','Cardoso',NULL),
+(2,'Rio de Janeiro','Isabela','Feminino','Athayde',NULL),
+(3,'São Paulo','André','Masculino','Costa',NULL),
+(4,'São Paulo','Fernanda','Feminino','Silva',NULL),
+(9,'Londrina','Thais','Feminino','Chiaretti',NULL),
+(10,'São Paulo','Bruno','Masculino','Marcos',NULL),
+(11,'Recife','Tatiana','Feminino','Barros',NULL),
+(12,'João Pessoa','Marina','Feminino','Cardoso',NULL),
+(13,'Banco da praça','Carlos','Masculino','Alberto',NULL),
+(14,'Ilha Grande','Alexandre','Masculino','Rios',NULL),
+(15,'Manaus','Eduarda','Feminino','Cabrini',NULL),
+(16,'Rio de Janeiro','Eliana','Feminino','Almeida',NULL);
